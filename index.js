7cfb67a57e352ae4bc0e1e30b46580d52b5dc727
Nick Payne <nick.payne89@gmail.com>
2019-01-29 14:40:14 +0800

    testing azure deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello deployment')
 });
 
 app.listen(8080, () => console.log('listening on port 8080'));
