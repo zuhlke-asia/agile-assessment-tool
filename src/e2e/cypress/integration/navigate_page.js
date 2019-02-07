8ad627ca2672ac71671a42e798265b6f73046658
Wu Xudong <wooxudong@gmail.com>
2019-02-07 17:22:45 +0800

    Added the complete test for the survey submission.

--- src/e2e/cypress/integration/navigate_page.js
+++ src/e2e/cypress/integration/navigate_page.js
@@ -1,15 +1,34 @@
-describe("My first Test", function () {
+describe("Navigate", function () {
     beforeEach(function(){
         cy.visit("http://localhost:8080");
+        cy.server();
+        cy.route('POST', '/api/userscores').as('submitForm')
     });
 
-    it("click first page", function () {
+    it("should Navigate all the pages without an error ", function () {
+
         cy.get('.progress span').contains("Page 1 of 4")
         cy.contains("Mostly Agile").click();
         cy.contains("Somewhat happy").click();
         cy.get('input[value="Next"]').click();
         cy.get('.progress span').contains("Page 2 of 4");
+        cy.get('select').select('Story Points');
+        cy.get('input[value="Next"]').click();
+        cy.get('.progress span').contains("Page 3 of 4");
+        cy.contains("5").click();
+        cy.get('input[value="Next"]').click();
+        cy.get('.progress span').contains("Page 4 of 4");
+        cy.get('input[aria-label="Company Name').type("Zuhlke");
+        cy.get('input[aria-label="Email').type("example@zuhlke.com");
+        cy.get('input[value="Complete"]').click();
+
+        cy.wait('@submitForm').then(xhr => {
+            const status = xhr.xhr.status;
+            expect(status).to.equal(200);
+        });
     })
+
+
 
 
 })
