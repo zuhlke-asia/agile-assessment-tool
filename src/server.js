c0dce106fb8c0107ffe73ba40db26a5396666042
Nick Payne <nick.payne89@gmail.com>
2019-02-11 10:29:08 +0800

    fix build

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-const app = require('./server/app');
+const _app = require('./server/app');
 const {connectToDb} = require('./server/db');
 
 (async () => {
     const db = await connectToDb();
     const port = process.env.PORT || 8080;
-    app(db).listen(port, () => console.log(`listening on port ${port}`));
+    const app = await _app(db);
+    await app.listen(port, () => console.log(`listening on port ${port}`));
 })();
