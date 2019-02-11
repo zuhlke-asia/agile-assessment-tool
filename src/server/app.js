48e0bb8b804d704b86d8b78f3cd6b49f34c74303
Nick Payne <nick.payne89@gmail.com>
2019-02-11 13:40:55 +0800

    fix favicon

--- src/server/app.js
+++ src/server/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const _app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
 _app.use(bodyParser.json()); // support json encoded bodies
 _app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 _app.use('/static', express.static(path.join(__dirname, '../../build/static')));
+_app.use(favicon(path.join(__dirname, '../../build/favicon.ico')));
 
 _app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../../build/index.html'));
