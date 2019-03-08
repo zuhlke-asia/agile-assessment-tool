373618bf464465ab7a0d3c0692f4a77392d8adc0
Wu Xudong <wooxudong@gmail.com>
2019-03-08 10:55:07 +0800

    Added the pollyfill URLSearchParam for edge.

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,16 @@
-const axios = require('axios');
-
-export default axios.create({
-
-});
+export const pollyFillUrlSearchParamForEdge = (w) => {
+  w.URLSearchParams = w.URLSearchParams || function (searchString) {
+    let self = this;
+    self.searchString = searchString;
+    self.get = function (name) {
+      let results = new RegExp('[?&]' + name + '=([^&#]*)').exec(self.searchString);
+      if (results == null) {
+        return null;
+      }
+      else {
+        return decodeURI(results[1]) || 0;
+      }
+    };
+  }
+};
 
