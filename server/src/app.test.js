7acca714d8a14e2e10429868bc19191661506c7b
Nathan Khoo <nathan.khoo@zuhlke.com>
2019-05-22 15:52:31 +0800

    Add timeout for mongodb-memory-server
    - Basic formatting

--- server/src/app.test.js
+++ server/src/app.test.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 let db;
 let server;
 
+// For mongodb-memory-server download
+jest.setTimeout(60000);
+
 beforeAll(async (done) => {
   process.env.NODE_ENV = 'test';
 
@@ -17,7 +20,8 @@ beforeAll(async (done) => {
 
 describe('The root path', () => {
   test('It should response the GET method', (done) => {
-    request(server).get('/')
+    request(server)
+      .get('/')
       .expect(200)
       .end(done);
   });
@@ -35,20 +39,25 @@ describe('The /api/survey path', () => {
       .set('Accept', 'application/json')
       .expect(200)
       .end(() => {
-        db.collection('userscores').findOne(payload).then((document) => {
-          expect(document.someQuestion).toBe(payload.someQuestion);
-          expect(document.date).toBeDefined();
-          done();
-        });
+        db.collection('userscores')
+          .findOne(payload)
+          .then((document) => {
+            expect(document.someQuestion).toBe(payload.someQuestion);
+            expect(document.date).toBeDefined();
+            done();
+          });
       });
   });
 });
 
 describe('The /api/surveyconfig path', () => {
   test('It should return the test config file if env is set to test', (done) => {
-    const expectedResponse = JSON.parse(fs.readFileSync(`${__dirname}/surveys/survey.test.json`, 'utf8'));
+    const expectedResponse = JSON.parse(
+      fs.readFileSync(`${__dirname}/surveys/survey.test.json`, 'utf8'),
+    );
 
-    request(server).get('/api/surveyconfig')
+    request(server)
+      .get('/api/surveyconfig')
       .expect(200)
       .expect((response) => {
         expect(response.body).toEqual(expectedResponse);
@@ -69,15 +78,16 @@ describe('The /api/feedback path', () => {
       .set('Accept', 'application/json')
       .expect(200)
       .end(() => {
-        db.collection('feedbacks').findOne(payload).then((document) => {
-          expect(document.feedback).toBe(payload.feedback);
-          expect(document.date).toBeDefined();
-          done();
-        });
+        db.collection('feedbacks')
+          .findOne(payload)
+          .then((document) => {
+            expect(document.feedback).toBe(payload.feedback);
+            expect(document.date).toBeDefined();
+            done();
+          });
       });
   });
 });
-
 
 afterAll((done) => {
   server.close(() => {
