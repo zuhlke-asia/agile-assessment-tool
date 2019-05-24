7eb6b093ccd11410738990569eba68d150a7aac6
Nathan Khoo <nathan.khoo@zuhlke.com>
2019-05-24 09:36:28 +0800

    Inline Network class

--- e2e/cypress/integration/complete_survey.js
+++ e2e/cypress/integration/complete_survey.js
@@ -1,56 +1,49 @@
-import Network from '../elements/CypressHttpRequest';
 import Survey from '../elements/Survey';
 
 describe('Complete Survey', () => {
-  let network;
-  const POST_SURVEY_ALIAS = 'postSurvey';
-
   before(() => {
-    network = new Network();
-    network.startServer();
-    network.registerRouteServices(POST_SURVEY_ALIAS);
+    cy.server();
+    cy.route('POST', '/api/survey').as('postSurvey');
   });
 
   beforeEach(() => {
-    network.visitHomePage();
+    cy.visit('/');
   });
 
   it('should fill out the survey and submit without an error', () => {
-    const survey = new Survey();
+    Survey.chooseAnswer('Mostly Agile');
+    Survey.chooseAnswer('Somewhat happy');
+    Survey.nextPage();
 
-    survey.chooseAnswer('Mostly Agile');
-    survey.chooseAnswer('Somewhat happy');
-    survey.nextPage();
+    Survey.shouldShowQuestion('What units do you use for estimation?');
+    Survey.previousPage();
+    Survey.shouldShowQuestion('How happy are you with your current software development process?');
+    Survey.nextPage();
 
-    survey.shouldShowQuestion('What units do you use for estimation?');
-    survey.previousPage();
-    survey.shouldShowQuestion('How happy are you with your current software development process?');
-    survey.nextPage();
+    Survey.selectAnswerFromDropdown('Story Points');
+    Survey.nextPage();
 
-    survey.selectAnswerFromDropdown('Story Points');
-    survey.nextPage();
+    Survey.selectRating('1');
+    Survey.nextPage();
 
-    survey.selectRating('1');
-    survey.nextPage();
+    Survey.chooseAnswer('No');
+    Survey.shouldNotShowQuestion('How often do you reach your sprint goals?');
+    Survey.chooseAnswer('Yes');
+    Survey.shouldShowQuestion('How often do you reach your sprint goals?');
+    Survey.chooseAnswer('We achieve them sometimes');
+    Survey.nextPage();
 
-    survey.chooseAnswer('No');
-    survey.shouldNotShowQuestion('How often do you reach your sprint goals?');
-    survey.chooseAnswer('Yes');
-    survey.shouldShowQuestion('How often do you reach your sprint goals?');
-    survey.chooseAnswer('We achieve them sometimes');
-    survey.nextPage();
+    Survey.enterTextBox('Company Name', 'Zuhlke');
+    Survey.enterTextBox('Email', 'example@zuhlke.com');
+    Survey.submit();
 
-    survey.enterTextBox('Company Name', 'Zuhlke');
-    survey.enterTextBox('Email', 'example@zuhlke.com');
-    survey.submit();
+    Survey.shouldShowResultPage();
+    Survey.shouldShowPercentage('Agility', 80);
+    Survey.shouldShowPercentage('Estimation', 100);
+    Survey.shouldShowPercentage('Mood', 20);
+    Survey.shouldShowNumberOfLinks(6);
+    Survey.shouldShowNumberOfProfiles(2);
 
-    survey.shouldShowResultPage();
-    survey.shouldShowPercentage('Agility', 80);
-    survey.shouldShowPercentage('Estimation', 100);
-    survey.shouldShowPercentage('Mood', 20);
-    survey.shouldShowNumberOfLinks(6);
-    survey.shouldShowNumberOfProfiles(2);
-
-    survey.giveFeedback('This was a great survey!');
+    Survey.giveFeedback('This was a great survey!');
   });
 });
