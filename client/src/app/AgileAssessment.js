52c5af90fdff9dd639924d79b3cfee9ff1ada8a4
Mike Leneweit <mike.leneweit@zuhlke.com>
2019-05-30 10:42:54 +0800

    Removed jquery
    (cherry picked from commit d057a60081aca8fbadb05ffc1b6c97fa96170484)
    (cherry picked from commit e42c787769cc7b4ea473432026b01e6428316154)

--- client/src/app/AgileAssessment.js
+++ client/src/app/AgileAssessment.js
@@ -2,43 +2,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as Survey from 'survey-react';
-import * as widgets from 'surveyjs-widgets';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'survey-react/survey.css';
-import 'jquery-ui/themes/base/all.css';
 import 'nouislider/distribute/nouislider.css';
 import 'select2/dist/css/select2.css';
 import 'bootstrap-slider/dist/css/bootstrap-slider.css';
 
-import 'jquery-bar-rating/dist/themes/css-stars.css';
-
-import $ from 'jquery';
-// eslint-disable-next-line import/extensions
-import 'jquery-ui/ui/widgets/datepicker.js';
 // eslint-disable-next-line import/extensions
 import 'select2/dist/js/select2.js';
-import 'jquery-bar-rating';
-
-
-widgets.icheck(Survey, $);
-widgets.select2(Survey, $);
-widgets.inputmask(Survey);
-widgets.jquerybarrating(Survey, $);
-widgets.jqueryuidatepicker(Survey, $);
-widgets.nouislider(Survey);
-widgets.select2tagbox(Survey, $);
-widgets.signaturepad(Survey);
-widgets.sortablejs(Survey);
-widgets.ckeditor(Survey);
-widgets.autocomplete(Survey, $);
-widgets.bootstrapslider(Survey);
 
 class AgileAssessment extends React.Component {
   componentWillMount() {
     import('icheck');
-    // eslint-disable-next-line no-multi-assign
-    window.$ = window.jQuery = $;
   }
 
   shouldComponentUpdate() {
